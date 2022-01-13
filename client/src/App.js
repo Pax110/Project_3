f0dd7841842e8251f8ab7734f7221ce2442fb8c3
Michlovin <michb5@hotmail.com>
2022-01-13 14:08:49 -0700

    created footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import LandingImage from "./components/landingimage/LandingImage.js";
 import Navbar from "./components/navigation/Navbar.js";
+import Footer from "./components/footer/Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingImage />
+      <Footer/>
     </div>
   );
 }
