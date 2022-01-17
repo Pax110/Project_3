1917b2c3a3d5832dbe17c1f85f83031f5835214e
Pax <tripathipankil@gmail.com>
2022-01-17 13:18:37 -0700

    Firebase-config

---
+++ client/src/components/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+const config = {
+  apiKey: "AIzaSyDa0LD1FY_-s8G9Sq__4jyQaHnCtwVs4O4",
+  authDomain: "project-c1c8c.firebaseapp.com",
+  projectId: "project-c1c8c",
+  storageBucket: "project-c1c8c.appspot.com",
+  messagingSenderId: "828436672252",
+  appId: "1:828436672252:web:42afbb782a929e5f497179",
+  measurementId: "G-D18T0N5LVG",
+};
+
+console.log(`config is : `, config);
+const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export default app;
