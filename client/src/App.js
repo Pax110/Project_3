2e200007f52f0b4bb6cbadd0cc1fa6fba777b725
Pax <tripathipankil@gmail.com>
2022-01-19 06:32:57 -0700

    firebaseAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,43 @@
-import { Routes, Route } from "react-router-dom"
+
 import LandingImage from "./components/landingimage/LandingImage.js";
 import Navbar from "./components/navigation/Navbar.js";
 import Footer from "./components/footer/Footer.js";
-import SignInScreen from "./components/Signin.js";
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./components/context/UserAuthContext";
+
 function App() {
   return (
-    <div className="App">
-      <SignInScreen />
-      
-
-
-        
-      
-      
-    </div>
+    <Container style={{ width: "400px" }}>
+      <Navbar />
+      <Row>
+        <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              
+            </Routes>
+          </UserAuthContextProvider>
+        </Col>
+      </Row>
+      <Footer />
+    </Container>
   );
 }
 
 export default App;
+
---
+++ client/src/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  // apiKey: <YOURAPIKEY>,
+  // authDomain: <YOURAUTHDOAMIN>,
+  // projectId: <YOURPROJECTID>,
+  // storageBucket: <YOURSTORAGEBUCKET>,
+  // messagingSenderId: <YOURMESSAGINGSENDERID>,
+  // appId: <YOURAPPID>,
+  apiKey: "AIzaSyDa0LD1FY_-s8G9Sq__4jyQaHnCtwVs4O4",
+    authDomain: "project-c1c8c.firebaseapp.com",
+    projectId: "project-c1c8c",
+    storageBucket: "project-c1c8c.appspot.com",
+    messagingSenderId: "828436672252",
+    appId: "1:828436672252:web:42afbb782a929e5f497179",
+    measurementId: "G-D18T0N5LVG"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
