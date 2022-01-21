06c763bd36d378ca08faeb2d95a7080315303b34
Michlovin <michb5@hotmail.com>
2022-01-20 17:45:45 -0700

    push

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import Footer from "./components/footer/Footer.js";
 import RestoDisplayCard from "./components/restaurant/RestoDisplayCard.js";
 import MenuDisplayCard from "./components/restaurant/MenuDisplayCard.js";
 
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <LandingImages/>
+      <Navbar/>
+      <LandingImage/>
       <RestoDisplayCard/>
       <Footer/>
       <MenuDisplayCard/>
-      <MenuDisplayCardTwo/>
+      
     </div>
   );
 }
 
 export default App;
+
--- client/src/components/restaurant/MenuDisplayCard.js
+++ client/src/components/restaurant/MenuDisplayCard.js
@@ -2,15 +2,18 @@ import * as React from 'react';
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { CardActionArea } from '@mui/material';
+import SuccessAlert from './Alerts';
 
 
-const MenuDisplayCard = () => {
-    const [restaurants, setRestaurants] = useState([]);
+const MenuDisplayCardTwo = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const { initializeApp } = require('firebase/app');
+  const { getFirestore, collection, getDocs } = require('firebase/firestore');
+
     const restaurantsCollectionRef = collection(db, "restaurants");
 
     useEffect(() => {
@@ -22,28 +25,95 @@ const MenuDisplayCard = () => {
         getRestaurants();
       }, []);
     console.log(restaurants)
-
-
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="menu-item"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <ImageList sx={{ width: 500, height: 450 }}>
+      {itemData.map((item) => (
+        <CardActionArea>
+          <SuccessAlert> </SuccessAlert>
+          {/* OnClick={!isStatic ? () => MenuItemSelected() : null} */}
+
+        <ImageListItem key={item.img}>
+          <img
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>by: {item.author}</span>}
+            position="below"
+          />
+        </ImageListItem>
+        </CardActionArea>
+      ))}
+    </ImageList>
   );
 }
-export default MenuDisplayCard;
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+    author: '@bkristastucchio',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+    author: '@rollelflex_graphy726',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+    author: '@helloimnik',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+    author: '@nolanissac',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+    author: '@hjrc33',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+    author: '@arwinneil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+    author: '@tjdragotta',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+    author: '@katie_wasserman',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+    author: '@silverdalex',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+    author: '@shelleypauls',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+    author: '@peterlaster',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+    author: '@southside_customs',
+  },
+];
+
+export default MenuDisplayCardTwo;
+
+
