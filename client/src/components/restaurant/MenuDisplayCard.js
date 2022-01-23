f28d4247afeb14e99ed30fa1b1b50b21b3d72e28
Michlovin <michb5@hotmail.com>
2022-01-22 18:48:41 -0700

    targeting menu values

--- client/src/components/restaurant/MenuDisplayCard.js
+++ client/src/components/restaurant/MenuDisplayCard.js
@@ -1,127 +1,63 @@
-import * as React from 'react';
+import * as React from "react";
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { collection, getDocs } from "firebase/firestore";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import { CardActionArea } from '@mui/material';
-import Toast from './Toast';
-
-
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import { CardActionArea } from "@mui/material";
+import Toast from "./Toast";
 
 const MenuDisplayCardTwo = () => {
   const [restaurants, setRestaurants] = useState([]);
   const [showToast, setShowToast] = useState(false);
-  const { initializeApp } = require('firebase/app');
-  const { getFirestore, collection, getDocs } = require('firebase/firestore');
+  const { initializeApp } = require("firebase/app");
+  const { getFirestore, collection, getDocs } = require("firebase/firestore");
+  const restaurantsCollectionRef = collection(db, "restaurants");
 
-    const restaurantsCollectionRef = collection(db, "restaurants");
-
-    useEffect(() => {
-        const getRestaurants = async () => {
-          const data = await getDocs(restaurantsCollectionRef);
-          console.log(data.docs)
-          setRestaurants(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        };
-        getRestaurants();
-      }, []);
-    console.log(restaurants)
+  useEffect(() => {
+    const getRestaurants = async () => {
+      const data = await getDocs(restaurantsCollectionRef);
+      console.log(data.docs);
+      setRestaurants(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getRestaurants();
+  }, []);
+  console.log(restaurants);
   return (
     <ImageList sx={{ width: 500, height: 450 }}>
-      {itemData.map((item) => (
+      {restaurants.map((restaurant) => (
         <CardActionArea>
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <button onClick={()=>{
-            setShowToast(true)
-            setTimeout(()=>{
-            setShowToast(false)
-            },1500)
-          }}>Add to Cart</button>
-          {showToast && <Toast message="Sucessfully Added"/> }
-         
-  
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>by: {item.author}</span>}
-            position="below"
-          />
-        </ImageListItem>
+          <ImageListItem key={restaurant.photoURL}>
+            <img
+              src={`${restaurant.photoURL}?w=248&fit=crop&auto=format`}
+              srcSet={`${restaurant.photoURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={restaurant.name}
+              loading="lazy"
+            />
+            <button
+              onClick={() => {
+                setShowToast(true);
+                setTimeout(() => {
+                  setShowToast(false);
+                }, 1500);
+              }}
+            >
+              Add to Cart
+            </button>
+            {showToast && <Toast message="Sucessfully Added" />}
+
+            <ImageListItemBar
+              title={restaurant.menu.menu.appetizers.name}
+              subtitle={
+                <span>price: {restaurant.menu.menu.appetizers.price}</span>
+              }
+              position="below"
+            />
+          </ImageListItem>
         </CardActionArea>
       ))}
     </ImageList>
   );
-}
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-  },
-];
-
+};
 export default MenuDisplayCardTwo;
-
-
