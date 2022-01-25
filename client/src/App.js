3a5c56a7278ddb36fddb98a7950a7c4cdec68d6f
kpow07 <kpow@shaw.ca>
2022-01-25 14:41:12 -0700

    form in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import Navbar from "./components/navigation/Navbar.js";
 import Footer from "./components/footer/Footer.js";
 import { Container, Row, Col } from "react-bootstrap";
@@ -11,49 +10,60 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider } from "./components/context/UserAuthContext";
 import UpdateProfile from "./components/UpdateProfile.js";
 import ForgotPassword from "./components/ForgotPassword.js";
+import RestoSignUpForm from "./components/restaurant/RestoSignUpForm";
 
 import LandingPage from "./components/LandingPage.js";
 import Profile from "./components/Profile.js";
 function App() {
   return (
-    <Container >
+    <Container>
       <Navbar />
-      <Container >
-      <Row>
-        <Col>
-          <UserAuthContextProvider>
-            <Routes>
-              <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/update-profile" element={<ProtectedRoute><UpdateProfile /></ProtectedRoute>}/>
-              <Route path="/signin" element={<Login />} />
+      <Container>
+        <Row>
+          <Col>
+            <UserAuthContextProvider>
+              <Routes>
+                <Route
+                  path="/home"
+                  element={
+                    <ProtectedRoute>
+                      <Home />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/update-profile"
+                  element={
+                    <ProtectedRoute>
+                      <UpdateProfile />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/signin" element={<Login />} />
+                <Route
+                  path="/restaurant-signup"
+                  element={<RestoSignUpForm />}
+                />
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/signup" element={<Signup />} />
 
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/signup" element={<Signup />} />
-              
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>} />
-              
-              
-
-              
-            </Routes>
-          </UserAuthContextProvider>
-        </Col>
-      </Row>
-      
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+            </UserAuthContextProvider>
+          </Col>
+        </Row>
       </Container>
       <Footer />
-     
     </Container>
   );
 }
 
 export default App;
-
--- client/src/components/restaurant/RestoSignUpForm.js
+++ client/src/components/restaurant/RestoSignUpForm.js
@@ -1,21 +1,109 @@
-import { Form, Container } from "react-bootstrap";
+import { Form, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
 
 const RestoSignUpForm = () => {
   return (
     <>
-      <Container style={{ width: "400px" }}>
+      <Container style={{ width: "600px" }}>
         <div className="p-4 box">
-          <h2 className="mb-3"> Restaurant </h2>
+          <h2 className="mb-3 text-center">Join Culinary Collective!</h2>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Email address" />
+            <Form.Group className="mb-3" controlId="formRestoName">
+              <Form.Label>Business Name:</Form.Label>
+              <Form.Control type="name" placeholder="Business Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Label>Business Type:</Form.Label>
+              <Row>
+                <Col>
+                  {" "}
+                  <Form.Check type="radio" value="home" label="Home Kitchen" />
+                </Col>
+                <Col>
+                  {" "}
+                  <Form.Check
+                    type="radio"
+                    value="commissary"
+                    label="Commissary Kitchen"
+                  />{" "}
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Address:</Form.Label>
+              <Form.Control placeholder="1234 Main Street" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3" controlId="formGridAddress2">
+              <Form.Label>Address 2:</Form.Label>
+              <Form.Control placeholder="Apartment, Studio, Floor, or Prep Area (if applicable)" />
             </Form.Group>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City:</Form.Label>
+                <Form.Control />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridProvince">
+                <Form.Label>Province or Territory:</Form.Label>
+                <Form.Select defaultValue="Choose...">
+                  <option>Choose...</option>
+                  <option value="Alberta">Alberta</option>
+                  <option value="British Columbia">British Columbia</option>
+                  <option value="Manitoba">Manitoba</option>
+                  <option value="New Brunswick">New Brunswick</option>
+                  <option value="Newfoundland and Labrador">
+                    Newfoundland and Labrador
+                  </option>
+                  <option value="Northwest Territories">
+                    Northwest Territories
+                  </option>
+                  <option value="Nova Scotia">Nova Scotia</option>
+                  <option value="Nunavut">Nunavut</option>
+                  <option value="Ontario">Ontario</option>
+                  <option value="Prince Edward Island">
+                    Prince Edward Island
+                  </option>
+                  <option value="Quebec">Quebec</option>
+                  <option value="Saskatchewan">Saskatchewan</option>
+                  <option value="Yukon">Yukon</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPostalCode">
+                <Form.Label>Postal Code:</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Row>
+                <Col>
+                  <Form.Label>Owner Contact:</Form.Label>
+                  <Form.Control type="firstName" placeholder="First Name" />
+                </Col>
+                <Col>
+                  <Form.Label>
+                    <br />
+                  </Form.Label>
+                  <Form.Control type="lastName" placeholder="Last Name" />
+                </Col>
+              </Row>
+            </Form.Group>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Phone Number:</Form.Label>
+                <Form.Control type="phoneNumber" placeholder="Phone Number" />
+              </Form.Group>
+            </Row>
 
             <div className="d-grid gap-2">
               <Button variant="primary" type="Submit">
@@ -24,7 +112,9 @@ const RestoSignUpForm = () => {
             </div>
           </Form>
         </div>
-        <div className="p-4 box mt-3 text-center">Already have an account?</div>
+        <div className="p-4 box mt-3 text-center">
+          Already have an account? <Link to="/signin">Log In</Link>
+        </div>
       </Container>
     </>
   );
