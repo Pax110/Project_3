8516fa99cdb564f10a58c61531b29b5a70f63bda
Michlovin <michb5@hotmail.com>
2022-01-25 14:22:39 -0700

    updating file

--- client/src/components/landingimage/LandingImage.js
+++ client/src/components/landingimage/LandingImage.js
@@ -3,7 +3,7 @@ import header from "./food1.jpg";
 import "./landing.css";
 
 const LandingImage = () => {
-  return <img src={header} alt="Food" class="responsive" />;
+  return <img src={header} alt="Food" className="responsive" />;
 };
 
 export default LandingImage;
