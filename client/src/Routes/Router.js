ec77227fc5d6b206103aba1e82e2792a8518fdba
Michlovin <michb5@hotmail.com>
2022-01-26 15:36:54 -0700

    menu page routed

--- client/src/Routes/Router.js
+++ client/src/Routes/Router.js
@@ -1,44 +1,65 @@
-import React from 'react';
+import React from "react";
 
 import ProtectedRoute from "../components/ProtectedRoute";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage.js";
 import ProfileTest from "../components/ProfileTest.js";
-import LoginPage from '../pages/LoginPage';
-import SignupPage from '../pages/SignupPage';
-import ForgotPasswordPage from '../pages/ForgotPasswordPage';
-import HomePage from '../pages/HomePage';
-import ProfilePage from '../pages/ProfilePage';
-import UpdateProfilePage from '../pages/UpdateProfilePage';
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
+import ForgotPasswordPage from "../pages/ForgotPasswordPage";
+import HomePage from "../pages/HomePage";
+import ProfilePage from "../pages/ProfilePage";
+import UpdateProfilePage from "../pages/UpdateProfilePage";
+import MenuPage from "../pages/MenuPage";
 
 const Router = () => {
-  return <div>
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/update-profile"
+          element={
+            <ProtectedRoute>
+              <UpdateProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/signin" element={<LoginPage />} />
 
-            <Routes>
-              <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <HomePage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/update-profile" element={<ProtectedRoute><UpdateProfilePage /></ProtectedRoute>}/>
-              <Route path="/signin" element={<LoginPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              
-              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-              <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-              
-              
-              <Route path="/profile" element={<ProtectedRoute><ProfileTest/></ProtectedRoute>} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
 
-              
-            </Routes>
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfileTest />
+            </ProtectedRoute>
+          }
+        />
 
-  </div>;
+        <Route path="/menu" element={<MenuPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Router;
---
+++ client/src/pages/MenuPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuDisplayCard from "../components/restaurant/MenuDisplayCard";
+
+const MenuPage = () => {
+  return (
+    <div>
+      <MenuDisplayCard />
+    </div>
+  );
+};
+
+export default MenuPage;
