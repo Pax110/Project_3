84d9b0bb9950462766458a40a204a581aecf5066
kpow07 <kpow@shaw.ca>
2022-01-27 12:05:38 -0700

    added page and route

---
+++ client/src/pages/RestoSignUpPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RestoSignUp from "../components/forms/RestoSignUpForm";
+
+const RestoSignUpPage = () => {
+  return (
+    <div>
+      <RestoSignUp />
+    </div>
+  );
+};
+
+export default RestoSignUpPage;
