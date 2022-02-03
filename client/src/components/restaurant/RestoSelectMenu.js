60de4cf3338fc443d54310e9c038e10f752c3fb2
kpow07 <kpow@shaw.ca>
2022-02-02 17:05:11 -0700

    dashboard with buttons

--- client/src/components/restaurant/RestoSelectMenu.js
+++ client/src/components/restaurant/RestoSelectMenu.js
@@ -1,35 +1,49 @@
 import { collection, getDocs, query, where } from "firebase/firestore";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUserAuth } from "../context/UserAuthContext";
 import { useFirebase } from "../FirebaseProvider";
+import { Form, Col } from "react-bootstrap";
 
 const RestoSelectMenu = () => {
   const { db } = useFirebase();
   const { user } = useUserAuth();
+  const [restoName, setRestoName] = useState([]);
+  const [restoData2, setRestoData2] = useState({});
 
-  const getData = async () => {
-    let restoRef = collection(db, "restaurants");
-    let q = query(restoRef, where("ownerUid", "==", user.uid));
-    let querySnap = await getDocs(q);
-    console.log(q);
-    console.log("the restos we own are:");
-    querySnap.forEach((doc) => {
-      let restoData = doc.data();
-      console.log(`name: ${restoData.name}`);
-    });
-  };
+  useEffect(() => {
+    const getData = async () => {
+      let restoRef = collection(db, "restaurants");
+      let q = query(restoRef, where("ownerUid", "==", user.uid));
+      let querySnap = await getDocs(q);
+      console.log(q);
+      console.log("the restos we own are:");
+      querySnap.forEach((doc) => {
+        let restoData = doc.data();
+        console.log(`name: ${restoData.name}`);
+        setRestoData2(restoData);
+      });
+    };
+    getData();
+  }, []);
+
   return (
     <div>
-      <button
-        variant="primary"
-        type="button"
-        onClick={() => {
-          console.log("clicked");
-          getData();
-        }}
-      >
-        Press here for data
-      </button>
+      {console.log("Resto Data2:", restoData2)}
+
+      <Form.Group as={Col} controlId="formGridProvince">
+        <Form.Label>Select Restaurant:</Form.Label>
+        <Form.Select
+          // defaultValue="Choose..."
+          type="province"
+          value={restoData2.name}
+          onChange={(e) => setRestoName(e.target.value)}
+        >
+          <option value="Choose...">Choose...</option>
+          <option value={restoName}>{restoData2.name}</option>
+          <option value={restoName}>{restoData2.name}</option>
+          <option value={restoName}>{restoData2.name}</option>
+        </Form.Select>
+      </Form.Group>
     </div>
   );
 };
--- client/src/pages/RestoDashboardPage.js
+++ client/src/pages/RestoDashboardPage.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import RestoSelectMenu from "../components/restaurant/RestoSelectMenu";
+
+//Need to add id and key to links
 
 const RestoDashboardPage = () => {
   return (
     <div>
       <RestoSelectMenu />
-      Hi I am the dashboard
+      <Link to="/restaurants/editprofile/">
+        <Button variant="primary" style={{ backgroundColor: "#feaa00", borderColor: "#feaa00" }}>
+          Account Details
+        </Button>
+      </Link>
+      <Link to="/restaurants/editmenu/">
+        <Button variant="primary" style={{ backgroundColor: "#feaa00", borderColor: "#feaa00" }}>
+          Menu Details
+        </Button>
+      </Link>
+      <h1> Hi I am the dashboard</h1>
     </div>
   );
 };
