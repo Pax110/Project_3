1bcc39e87d08e8ae8efb5f72b40c9aa8af08453f
Michlovin <michb5@hotmail.com>
2022-02-07 16:59:40 -0700

    text

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -16,12 +16,11 @@ const Signup = () => {
     setError("");
     try {
       const resp = await signUp(email, password);
-      
+
       const user = resp.user;
-      
+
       await createUserDocument(user);
-      
-      
+
       navigate("/profile");
     } catch (err) {
       setError(err.message);
@@ -30,37 +29,39 @@ const Signup = () => {
 
   return (
     <>
-    <Container style={{width: "400px"}}>
-      <div className="p-4 box">
-        <h2 className="mb-3"> Signup</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+      <Container style={{ width: "400px" }}>
+        <div className="p-4 box">
+          <h2 className="mb-3"> Signup</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <h5>Email</h5>
+              <Form.Control
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <h5>Password</h5>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
 
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Sign up
-            </Button>
-          </div>
-        </Form>
-      </div>
-      <div className="p-4 box mt-3 text-center">
-        Already have an account? <Link to="/signin">Log In</Link>
-      </div>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="Submit">
+                Sign up
+              </Button>
+            </div>
+          </Form>
+        </div>
+        <div className="p-4 box mt-3 text-center">
+          Already have an account? <Link to="/signin">Log In</Link>
+        </div>
       </Container>
     </>
   );
