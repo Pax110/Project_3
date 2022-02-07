4e7c3df66cf8253782d2e40549abbcad7dbc139d
kpow07 <kpow@shaw.ca>
2022-02-07 12:02:11 -0700

    pics n stuff

--- client/src/components/restaurant/RestoDisplayCard.js
+++ client/src/components/restaurant/RestoDisplayCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import background from "../landingimage/food1.jpg";
 import { useState, useEffect } from "react";
 import { useFirebase } from "../FirebaseProvider";
 import { collection, getDocs } from "firebase/firestore";
@@ -7,7 +8,7 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import IconButton from "@mui/material/IconButton";
-import { CardMedia } from "@mui/material";
+import { CardMedia, Container } from "@mui/material";
 
 const RestoDisplayCard = () => {
   const { db } = useFirebase();
@@ -29,32 +30,40 @@ const RestoDisplayCard = () => {
   }, []);
 
   return (
-    <ImageList cols={4}>
-      {restaurants.map((restaurant) => (
-        <Link to={`/menu/${restaurant.DOC_ID}`} key={restaurant.DOC_ID}>
-          <ImageListItem onClick={() => {}} key={restaurant.photoURL}>
-            <CardMedia
-              component="img"
-              height="250"
-              src={`${restaurant.photoURL}?w=248&fit=crop&auto=format`}
-              srcSet={`${restaurant.photoURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={restaurant.name}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              title={restaurant.name}
-              subtitle={restaurant.description}
-              actionIcon={
-                <IconButton
-                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                  aria-label={`info about ${restaurant.name}`}
-                ></IconButton>
-              }
-            />
-          </ImageListItem>
-        </Link>
-      ))}
-    </ImageList>
+    <Container
+      style={{
+        width: "auto",
+        backgroundColor: "rgba(254, 170, 0, 0.6)",
+      }}
+    >
+      <h1 className="p-4 box mt-3 text-center">Browse Local Cuisines</h1>
+      <ImageList cols={4}>
+        {restaurants.map((restaurant) => (
+          <Link to={`/menu/${restaurant.DOC_ID}`} key={restaurant.DOC_ID}>
+            <ImageListItem onClick={() => {}} key={restaurant.photoURL}>
+              <CardMedia
+                component="img"
+                height="250"
+                src={`${restaurant.photoURL}?w=248&fit=crop&auto=format`}
+                srcSet={`${restaurant.photoURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={restaurant.name}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={restaurant.name}
+                subtitle={restaurant.description}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                    aria-label={`info about ${restaurant.name}`}
+                  ></IconButton>
+                }
+              />
+            </ImageListItem>
+          </Link>
+        ))}
+      </ImageList>
+    </Container>
   );
 };
 
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import LandingImage from '../components/landingimage/LandingImage';
-import RestoDisplayCard from '../components/restaurant/RestoDisplayCard';
+import React from "react";
+import LandingImage from "../components/landingimage/LandingImage";
+import RestoDisplayCard from "../components/restaurant/RestoDisplayCard";
 
 function LandingPage() {
-  return <div style={{width: "1000px"}}>
+  return (
+    <div>
       <LandingImage />
       <RestoDisplayCard />
-      </div>;
+    </div>
+  );
 }
 
 export default LandingPage;
