ed101986a4065480a3cae67bf5a337597dffdc30
Pax <tripathipankil@gmail.com>
2022-02-09 06:08:04 -0700

    cleaning components

--- client/src/components/restaurant/RestoMenuEdit.js
+++ client/src/components/restaurant/RestoMenuEdit.js
@@ -8,52 +8,7 @@ import AddMainItem from "./AddMainItem";
 import AddDessertItem from "./AddDessertItem";
 import { useForm } from "react-hook-form";
 
-const ApptizerEdit = (props) => {
-  const {db} = FirebaseContext()
-  const { id } = useParams();
-  const item = props.item;
-  console.log("item", item);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ defaultValues: item });
-  const onSubmit = async (data2) =>{
 
-    console.log("data..updateDoc or query_specifi field nameand price", data2);
-    //button - currentResto ID_ updateDoc (ApI)_ change the spicific field's value
-    //google.. 
-    //send the the whole restaurant doc (updated) back to the "restaurant" doc
- 
-      let restoDocRef = doc(db, `restaurants/${id}`);
-      let data = await updateDoc(restoDocRef, "${menu.appetizers[0]}", {name: "zzzz", price: "$0.25"});
-      console.log("received data", data);
-    
-   
-
-  }
-  console.log("errors?", errors);
-  const temp =  {...register("name", { required: true })}
-  console.log("temp is",temp);
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="enter name"
-        {...temp}
-      />
-      <lable>Price: </lable>
-      <input
-        type="text"
-        placeholder={item.price}
-        {...register("price", { required: true })}
-      />
-      <button type="button" onClick={handleSubmit(onSubmit)}>
-        Update
-      </button>
-    </div>
-  )
-  }
 
 const RestoMenuEdit = () => {
   const { id } = useParams();
@@ -64,7 +19,7 @@ const RestoMenuEdit = () => {
 
   // const [tempName, setTempName] = useState("");
   // const [tempPrice, setTempPrice] = useState("");
-
+  
   //Using useForm
   const {
     register,
@@ -95,59 +50,28 @@ const RestoMenuEdit = () => {
     }
   }, [id]);
 
-  // const EditItemDisplay = (props) => {
-  //   const item = props.item;
-  //   return (
-  //     <Container
-  //       style={{
-  //         width: "400px",
-  //         backgroundColor: "#feaa00",
-  //       }}
-  //     >
-  //       <Form onSubmit={handleSubmit(onSubmit)}>
-  //         <Form.Group className="mb-3" controlId="formMenuItem">
-  //           <Form.Label>Name:</Form.Label>
-  //           <Form.Control
-  //             type="text"
-  //             placeholder="Name"
-  //             {...register("First name", { required: true, maxLength: 80 })}
-  //           />
-  //           <Form.Label>Price:</Form.Label>
-  //           <Form.Control
-  //             type="text"
-  //             placeholder="Price"
-  //             {...register("Price", { required: true })}
-  //           />
-  //           <br />
-  //           <input type="submit" />
-  //         </Form.Group>
-  //       </Form>
-  //     </Container>
-  //   );
-  // };
-
   return (
     <div>
       <h3> Menu: </h3>
       <h3> Appetizers:</h3>
-      {restaurant &&
+      {/* {restaurant &&
         restaurant.menu.appetizers.map((i, index) => (
           // <EditItemDisplay key={i.name} item={i}  />
           <ApptizerEdit key={i.name} item={i} /> //pass the path to updte + restoId, [`$index`]  //path: path=`menu.appetizers.` 
-        ))}
+        ))} */}
       {/* <AddAppItem /> //TO ADD ITEM */}
       <h3> Mains:</h3>
       {/* {restaurant &&
         restaurant.menu.mains.map((i) => (
           <EditItemDisplay key={i.name} item={i} />
-        ))}
+          ))}
       <AddMainItem /> */}
       <h3> Desserts:</h3>
       {/* {restaurant &&
         restaurant.menu.desserts.map((i) => (
           <EditItemDisplay key={i.name} item={i} />
-        ))}
-      <AddDessertItem /> */}
+          ))}
+        <AddDessertItem /> */}
       <h3>docId: {restaurant?.DOC_ID}</h3>
 
       <hr />
@@ -156,3 +80,76 @@ const RestoMenuEdit = () => {
 };
 
 export default RestoMenuEdit;
+
+// const EditItemDisplay = (props) => {
+//   const item = props.item;
+//   return (
+  //     <Container
+  //       style={{
+//         width: "400px",
+//         backgroundColor: "#feaa00",
+//       }}
+//     >
+//       <Form onSubmit={handleSubmit(onSubmit)}>
+//         <Form.Group className="mb-3" controlId="formMenuItem">
+//           <Form.Label>Name:</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Name"
+//             {...register("First name", { required: true, maxLength: 80 })}
+//           />
+//           <Form.Label>Price:</Form.Label>
+//           <Form.Control
+//             type="text"
+//             placeholder="Price"
+//             {...register("Price", { required: true })}
+//           />
+//           <br />
+//           <input type="submit" />
+//         </Form.Group>
+//       </Form>
+//     </Container>
+//   );
+// };
+
+// const ApptizerEdit = (props) => {
+//   const { db } = FirebaseContext();
+//   const { id } = useParams();
+//   const item = props.item;
+//   console.log("item", item);
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm({ defaultValues: item });
+//   const onSubmit = async (data2) => {
+//     console.log("data..updateDoc or query_specifi field nameand price", data2);
+//     //button - currentResto ID_ updateDoc (ApI)_ change the spicific field's value
+//     //google..
+//     //send the the whole restaurant doc (updated) back to the "restaurant" doc
+
+//     let restoDocRef = doc(db, `restaurants/${id}`);
+//     let data = await updateDoc(restoDocRef, "${menu.appetizers[0]}", {
+//       name: "zzzz",
+//       price: "$0.25",
+//     });
+//     console.log("received data", data);
+//   };
+// console.log("errors?", errors);
+// const temp = { ...register("name", { required: true }) };
+// console.log("temp is", temp);
+// return (
+//     <div>
+//       <input type="text" placeholder="enter name" {...temp} />
+//       <lable>Price: </lable>
+//       <input
+//         type="text"
+//         placeholder={item.price}
+//         {...register("price", { required: true })}
+//       />
+//       <button type="button" onClick={handleSubmit(onSubmit)}>
+//         Update
+//       </button>
+//     </div>
+//   );
+// };
