3402711dc6c7706499830ea74b5516eddf934efd
Pax <tripathipankil@gmail.com>
2022-02-09 10:03:48 -0700

    Greg's help

--- client/src/components/restaurant/EditRestoMenuForm.js
+++ client/src/components/restaurant/EditRestoMenuForm.js
@@ -1,43 +1,54 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
+const Appetizer = ({ register, index }) => {
+ 
+  const r = register(`name[${index}]`);
+  console.log("r is",r)
+  return (
+    <Container
+      style={{
+        width: "400px",
+        backgroundColor: "#feaa00",
+      }}
+    >
+      <label>Item Name: </label>
+      <br />
+      <input type="text"  {...register(`menu.appetizers[${index}].name`)}/>
+      <br />
+      <label>Item Price: </label>
+      <br />
+      <input type="text" {...register(`menu.appetizers[${index}].price`)} />
+      <input type="hidden" {...register(`menu.appetizers[${index}].type`)} value="appetizers" />
+      <br />
+
+      <br />
+    </Container>
+  );
+};
 const EditRestoMenuForm = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const docValue = props.document;
-  const id = props.id;
-    const [itemName, setItemName] = useState(null)
-    const [itemPrice,setItemPrice]= useState(null)
+
+  const onSubmit = (data) => console.log("data is",data);
+  const onError = (err) => console.log("error is",err);
+  console.log(errors);
   return (
     <div>
       <h3>EditRestoMenuForm</h3>
       <h3>Menu</h3>
       <div>Appetizer</div>
-      <Container
-        style={{
-          width: "400px",
-          backgroundColor: "#feaa00",
-        }}
-      >
-        <label>Item Name: </label>
-        <br />
-        <input
-          type="text"
-          defaultValue={docValue.menu.appetizers[0].name}
-          value={itemName}
-          onChange={(e)=>{setItemName(e.target.value)}}
-        />
-        <br />
-        <label>Item Price: </label>
-        <br />
-        <input
-          type="text"
-          defaultValue={docValue.menu.appetizers[0].price}
-          value={itemPrice}
-          onChange={(e)=>{setItemPrice(e.target.value)}}
-        />
-        <br />
-        <input type="submit" />
-        <br />
-      </Container>
+      {JSON.stringify(docValue.menu.appetizers)}
+      {docValue &&
+        docValue.menu.appetizers.map((data, index) => (
+          <Appetizer register={register} key={index} index={index} />
+        ))}
+      <button onClick={handleSubmit(onSubmit, onError)}>Update</button>
     </div>
   );
 };
--- client/src/components/restaurant/EditRestoMenuPage.js
+++ client/src/components/restaurant/EditRestoMenuPage.js
@@ -4,45 +4,43 @@ import { useParams } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
 import EditRestoMenuForm from "./EditRestoMenuForm";
 
-
-
-
-
 const EditRestoMenuPage = () => {
-    const {id} = useParams()
-    const {db} = useUserAuth()
-    const [restaurant,setRestaurant] = useState({})
-    useEffect(() => {
-        const getMenuData = async () => {
-          try {
-            let docRef = doc(db, "restaurants", id);
-            let docSnap = await getDoc(docRef);
-            if (!docSnap.exists()) {
-              console.log("No docs found");
-            } else {
-              let restoData = docSnap.data();
-              restoData.DOC_ID = docSnap.id;
-              setRestaurant(restoData);
-            }
-          } catch (ex) {
-            console.log("Firestore failure!", ex.message);
-          }
-        };
-        if (id) {
-          getMenuData();
+  const { id } = useParams();
+  const { db } = useUserAuth();
+  const [restaurant, setRestaurant] = useState(null);
+  useEffect(() => {
+    const getMenuData = async () => {
+      try {
+        let docRef = doc(db, "restaurants", id);
+        let docSnap = await getDoc(docRef);
+        if (!docSnap.exists()) {
+          console.log("No docs found");
+        } else {
+          let restoData = docSnap.data();
+          restoData.DOC_ID = docSnap.id;
+          setRestaurant(restoData);
         }
-      }, []);
-   
-    
-  return (
-    <div>
-       
-      <h3>EditRestoMenuPage</h3>
+      } catch (ex) {
+        console.log("Firestore failure!", ex.message);
+      }
+    };
+    if (id) {
+      getMenuData();
+    }
+  }, []);
+
+  if (restaurant) {
+    return (
+      <div>
+        <h3>EditRestoMenuPage</h3>
         <div>{JSON.stringify(restaurant)}</div>
-      {/* {restaurant.map((data)=>{console.log("data in the page",data)})} */}
-      <EditRestoMenuForm id={id} document={restaurant}/>
-    </div>
-  );
+        
+        <EditRestoMenuForm id={id} document={restaurant} />
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 };
 
 export default EditRestoMenuPage;
