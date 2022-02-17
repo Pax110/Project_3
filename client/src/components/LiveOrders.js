14df43e3db1062fe13975d1537d4b67978900e53
Pax <tripathipankil@gmail.com>
2022-02-17 07:41:56 -0700

    cart

--- client/src/components/LiveOrders.js
+++ client/src/components/LiveOrders.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { collection, doc, getDocs, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useUserAuth } from "./context/UserAuthContext";
 
 const LiveOrders = () => {
-  return (
-    <div>LiveOrders</div>
-  )
-}
+  const { db, user } = useUserAuth();
+  let collRef = collection(db, "orders");
+  let docRef = doc(collRef, user.uid);
+  const [orders, setOrders] = useState();
 
-export default LiveOrders
+ 
+
+    useEffect(()=>{
+      
+      const getData = async  () =>{
+        
+        try {
+          const q = query(docRef, where("user.uid", "==", user.uid));
+          const querySnapshot = await getDocs(q);
+          console.log("querySnapShot", querySnapshot);
+          setOrders(querySnapshot.data())
+        
+        } catch (e) {
+          console.log("error", e.message);
+        }
+
+
+      }
+
+      getData()
+
+    },[user.uid])
+
+    
+    
+    
+    
+    
+  return <div>{JSON.stringify(orders)}</div>;
+};
+
+export default LiveOrders;
