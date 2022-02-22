182809f491e9206fdb889df1451f9063b7cc531c
Pax <tripathipankil@gmail.com>
2022-02-22 16:32:19 -0700

    Cards added

--- client/src/components/restaurant/RestoOrdersView.js
+++ client/src/components/restaurant/RestoOrdersView.js
@@ -2,13 +2,14 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useUserAuth } from "../context/UserAuthContext";
 import { useParams } from "react-router-dom";
+import { Button, Card, ListGroup } from "react-bootstrap";
 
 const LiveOrders = () => {
   const { db, user } = useUserAuth();
 
   const [orders, setOrders] = useState();
   const { id } = useParams();
-  console.log("restoId",id)
+  console.log("restoId", id);
   useEffect(() => {
     const getData = async () => {
       try {
@@ -16,7 +17,7 @@ const LiveOrders = () => {
 
         const q = query(collRef, where("restaurantId", "==", id));
         const querySnapshot = await getDocs(q);
-        console.log("querySnapShop",querySnapshot)
+        console.log("querySnapShop", querySnapshot);
         console.log("querySnapShot", querySnapshot);
         let newData = querySnapshot.docs.map((doc) => ({
           ...doc.data(),
@@ -30,14 +31,62 @@ const LiveOrders = () => {
 
     getData();
   }, [user.uid]);
+  console.log("orders..", orders);
 
-  return (
-    <div>
-      <div>Orders</div>
-      <div>{JSON.stringify(orders)}</div>
-      
-    </div>
-  );
+  const orderStyle = {
+    display: "flex",
+  };
+
+  if (orders) {
+    return (
+      <div>
+        {orders.map((order) => (
+          <div>
+            <Card>
+              <Card.Body>
+                <Card.Title>Order Number: {order.orderId}</Card.Title>
+                <Card.Text>
+                {order.orderItems.map((item) => (
+                    <>
+                      <span>{item.name}</span>
+                      <span> x </span>
+                      <span>{item.qty}</span><br></br>
+                      
+                      
+                    </>
+                  ))}
+                  <hr></hr>
+                  <p>Order Total: ${order.orderTotal}</p>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+            {/* <Card>
+              <Card.Header>{order.DOC_ID}</Card.Header>
+              <Card.Body>
+                <Card.Title> Order Number: {order.orderId}</Card.Title>
+                <Card.Text>
+                {order.orderItems.map((item) => (
+                    <>
+                      <span>{item.name}</span><br></br>
+                      
+                    </>
+                  ))}
+                  <p>Order Total: ${order.orderTotal}</p>
+                </Card.Text>
+              </Card.Body>
+            </Card> */}
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div>No orders yet..We'll celebrate your first order together!</div>
+      </>
+    );
+  }
 };
 
 export default LiveOrders;
