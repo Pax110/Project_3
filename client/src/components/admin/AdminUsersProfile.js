5863611998c8d7f5d162bfe658842a913d7770c0
Pax <tripathipankil@gmail.com>
2022-02-22 19:11:56 -0700

    Admin orderspage added

--- client/src/components/admin/AdminUsersProfile.js
+++ client/src/components/admin/AdminUsersProfile.js
@@ -1,6 +1,6 @@
 import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { Tab, Tabs } from "react-bootstrap";
+import { Card, Tab, Tabs } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
 
@@ -8,7 +8,7 @@ const AdminUsersProfile = ({ data }) => {
   const { db, user } = useUserAuth();
   const [userProfile, setUserProfile] = useState([]);
   const { id } = useParams();
-
+  //Getting user profile data
   useEffect(() => {
     const getData = async () => {
       try {
@@ -30,6 +30,31 @@ const AdminUsersProfile = ({ data }) => {
 
     getData();
   }, [user.uid]);
+
+  //Getting user orders data
+  const [orders, setOrders] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let collRef = collection(db, "orders");
+
+        const q = query(collRef, where("customerId", "==", id));
+        const querySnapshot = await getDocs(q);
+        console.log("querySnapShot", querySnapshot);
+        let newData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          DOC_ID: doc.id,
+        }));
+        setOrders(newData);
+        console.log("Order history: ", newData);
+      } catch (e) {
+        console.log("error", e.message);
+      }
+    };
+
+    getData();
+  }, [user.uid]);
   console.log("userProfile", userProfile);
   return (
     <div>
@@ -42,21 +67,52 @@ const AdminUsersProfile = ({ data }) => {
               className="mb-3"
             >
               <Tab eventKey="profile" title="Profile">
-               
-               <div>First Name: {user.firstName}</div><br/>
-               <div>Last Name: {user.lastName}</div><br/>
-               <div>Email: {user.email}</div><br/>
-               <div>Phone: {user.phone}</div><br/>
-               <div>Address: {user.address} {user.city} {user.state} {user.zip}</div><br/>
-               
+                <div>First Name: {user.firstName}</div>
+                <br />
+                <div>Last Name: {user.lastName}</div>
+                <br />
+                <div>Email: {user.email}</div>
+                <br />
+                <div>Phone: {user.phone}</div>
+                <br />
+                <div>
+                  Address: {user.address} {user.city} {user.state} {user.zip}
+                </div>
+                <br />
               </Tab>
               <Tab eventKey="orders" title="Orders">
-                List of all the orders associated with this restaurant
+                {orders?.map((order) => (
+                  <Card>
+                    <Card.Header>Restaurant Name</Card.Header>
+                    <Card.Body>
+                      <Card.Title>
+                        Customer Order Number: {order.orderId}
+                      </Card.Title>
+                      <Card.Text>
+                        {order.orderItems.map((item) => (
+                          <>
+                            <span>{item.name}</span>
+                            <span>x</span>
+                            <span>{item.qty}</span>
+                            <span> </span>
+                            <br />
+                            <span>${item.price}</span>
+                            <br></br>
+                          </>
+                        ))}
+                        <hr></hr>
+                        <p>Order Total: ${order.orderTotal}</p>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                ))}
               </Tab>
-              {user.role != "Customer" && <Tab eventKey="uploads" title="View Uploads">
-                All the uploads the restaurnt has uploaded for us to approve or deny
-              </Tab>}
-             
+              {user.role != "Customer" && (
+                <Tab eventKey="uploads" title="View Uploads">
+                  All the uploads the restaurnt has uploaded for us to approve
+                  or deny
+                </Tab>
+              )}
             </Tabs>
           </div>
         );
