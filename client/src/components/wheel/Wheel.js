e2af4b507dfd151faf289db12f8dbea2187aaa71
Michlovin <michb5@hotmail.com>
2022-02-23 12:11:04 -0700

    wheel picker

---
+++ client/src/components/wheel/Wheel.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Wheel from 
+
+export default function Wheel() {
+  let container = document.querySelector(".container");
+  let btn = document.getElementById("spin");
+  let number = Math.ceil(Math.random() * 1000);
+
+  btn.onclick = function () {
+    container.style.transform = "rotate(" + number + "deg)";
+    number += Math.ceil(Math.random() * 1000);
+  };
+
+  return <div></div>;
+}
+
+
