7f7942b012ee5fc7345b33a687e5e44164f26270
Pax <tripathipankil@gmail.com>
2022-02-24 14:52:07 -0700

    deleted magicc8 from the main

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AboutUs from "../components/aboutus/AboutUs";
 import LandingImage from "../components/landingimage/LandingImage";
-import Magic8 from "../components/magic8/Magic8";
+
 import RestoDisplayCard from "../components/restaurant/RestoDisplayCard";
 import Filters from '../components/cart/Filters'
 function LandingPage() {
@@ -11,7 +11,7 @@ function LandingPage() {
       <LandingImage />
       <AboutUs />
       <RestoDisplayCard />
-      {/* <Magic8 /> */}
+    
     </div>
   );
 }
