04318ddf01548e65626eb4000f24ad4c806abb68
Michlovin <michb5@hotmail.com>
2022-02-28 15:27:42 -0700

    styling

--- client/src/components/restaurant/EditRestoMenuForm.js
+++ client/src/components/restaurant/EditRestoMenuForm.js
@@ -1,6 +1,6 @@
 import { collection, doc, updateDoc } from "firebase/firestore";
 import React, { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Card } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
@@ -30,7 +30,7 @@ const EditRestoMenuForm = (props) => {
 
     let collRef = collection(db, "restaurants");
     let docRef = doc(collRef, id);
-   
+
     // const receivedData = updateDoc(docRef, {
     //   menu: data.menu,
     // });
@@ -38,7 +38,7 @@ const EditRestoMenuForm = (props) => {
   };
 
   const onError = (err) => console.log("error is", err);
-
+  const myStyle = { fontFamily: "Bebas Neue" };
   return (
     //Needs styling here...
     <div
@@ -54,44 +54,47 @@ const EditRestoMenuForm = (props) => {
           backgroundColor: "rgba(225, 229, 235, 0.9)",
         }}
       >
-        <h3>Menu</h3>
+        <h1 className="p-4 box mt-3 text-center" style={myStyle}>
+          Menu
+        </h1>
+        <Card style={{ width: "50rem", margin: "auto", marginBottom: "10px" }}>
+          {/* APPETIZERS */}
 
-        {/* APPETIZERS */}
-
-        <h4>Appetizer</h4>
-        {/* {JSON.stringify(docValue.menu.appetizers)} */}
-        {docValue &&
-          docValue.menu.appetizers.map((data, index) => (
-            <>
-              <Appetizer register={register} key={index} index={index} />
-            </>
-          ))}
-        <Button>Add Item</Button>
-
+          <h4>Appetizer</h4>
+          {/* {JSON.stringify(docValue.menu.appetizers)} */}
+          {docValue &&
+            docValue.menu.appetizers.map((data, index) => (
+              <>
+                <Appetizer register={register} key={index} index={index} />
+              </>
+            ))}
+          <Button>Add Item</Button>
+        </Card>
         {/* MAINS */}
+        <Card style={{ width: "50rem", margin: "auto", marginBottom: "10px" }}>
+          <h4>Mains</h4>
+          {/* {JSON.stringify(docValue.menu.mains)} */}
 
-        <h4>Mains</h4>
-        {/* {JSON.stringify(docValue.menu.mains)} */}
-
-        {docValue &&
-          docValue.menu.mains.map((data, index) => (
-            <>
-              <Main register={register} key={index} index={index} />
-            </>
-          ))}
-        <Button>Add Item</Button>
-
+          {docValue &&
+            docValue.menu.mains.map((data, index) => (
+              <>
+                <Main register={register} key={index} index={index} />
+              </>
+            ))}
+          <Button>Add Item</Button>
+        </Card>
         {/* DESSERTS */}
-
-        <h4>Desserts</h4>
-        {/* {JSON.stringify(docValue.menu.desserts)} */}
-        {docValue &&
-          docValue.menu.desserts.map((data, index) => (
-            <>
-              <Dessert register={register} key={index} index={index} />
-            </>
-          ))}
-        <Button>Add Item</Button>
+        <Card style={{ width: "50rem", margin: "auto", marginBottom: "10px" }}>
+          <h4>Desserts</h4>
+          {/* {JSON.stringify(docValue.menu.desserts)} */}
+          {docValue &&
+            docValue.menu.desserts.map((data, index) => (
+              <>
+                <Dessert register={register} key={index} index={index} />
+              </>
+            ))}
+          <Button>Add Item</Button>
+        </Card>
         <Button onClick={handleSubmit(onSubmit, onError)}>Update</Button>
       </Container>
     </div>
