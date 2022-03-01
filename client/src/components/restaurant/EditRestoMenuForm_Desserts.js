385d5a979c7378834659645538c25a0ef2e8e775
Michlovin <michb5@hotmail.com>
2022-03-01 08:59:35 -0700

    buttons added to main and dessert card

--- client/src/components/restaurant/EditRestoMenuForm_Desserts.js
+++ client/src/components/restaurant/EditRestoMenuForm_Desserts.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Button, Container, Form } from "react-bootstrap";
+import React from "react";
+import { Button, Container, Form, Card } from "react-bootstrap";
+import { Delete } from "@styled-icons/fluentui-system-regular/Delete";
+import { ImageAdd } from "@styled-icons/boxicons-solid/ImageAdd";
+import { AddImageIcon } from "../icon/AddImageIcon";
 const Dessert = ({ register, index }) => {
-    // const r = register(`name[${index}]`);
-    // console.log("r is",r)
-    return (
-      <Container
-        style={{
-          width: "400px",
-          backgroundColor: "#feaa00",
-        }}
-      >
+  // const r = register(`name[${index}]`);
+  // console.log("r is",r)
+  return (
+    <Card
+      style={{
+        width: "400px",
+        backgroundColor: "white",
+      }}
+    >
+      <Card.Body>
         <label>Item Name: </label>
         <br />
         <input type="text" {...register(`menu.desserts[${index}].name`)} />
@@ -27,10 +31,36 @@ const Dessert = ({ register, index }) => {
           {...register(`menu.desserts[${index}].menuphoto`)}
           value="desserts"
         />
+        <Button
+          style={{
+            backgroundColor: "#feaa00",
+            borderColor: "#feaa00",
+            padding: "0.25rem",
+            margin: "3%",
+          }}
+        >
+          <AddImageIcon style={{ width: "30px", height: "30px" }} />
+          Upload Images
+        </Button>
         <br />
-  
+        <Button
+          variant="danger"
+          style={{
+            padding: "0.25rem",
+            margin: "3%",
+            top: "50%",
+            left: "50%",
+          }}
+          // onClick={handleDelete}
+        >
+          <Delete style={{ width: "30px", height: "30px" }} />
+          Delete
+        </Button>
         <br />
-      </Container>
-    );
-  };
-export default Dessert
+
+        <br />
+      </Card.Body>
+    </Card>
+  );
+};
+export default Dessert;
--- client/src/components/restaurant/EditRestoMenuForm_Mains.js
+++ client/src/components/restaurant/EditRestoMenuForm_Mains.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import { Button, Container, Form } from "react-bootstrap";
+import React from "react";
+import { Button, Container, Form, Card } from "react-bootstrap";
+import { Delete } from "@styled-icons/fluentui-system-regular/Delete";
+import { ImageAdd } from "@styled-icons/boxicons-solid/ImageAdd";
+import { AddImageIcon } from "../icon/AddImageIcon";
 
 const Main = ({ register, index }) => {
-    // const r = register(`name[${index}]`);
-    // console.log("r is",r)
-    return (
-      <Container
-        style={{
-          width: "400px",
-          backgroundColor: "#feaa00",
-        }}
-      >
+  // const r = register(`name[${index}]`);
+  // console.log("r is",r)
+  return (
+    <Card
+      style={{
+        width: "400px",
+        backgroundColor: "white",
+      }}
+    >
+      <Card.Body>
         <label>Item Name: </label>
         <br />
         <input type="text" {...register(`menu.mains[${index}].name`)} />
@@ -23,16 +27,42 @@ const Main = ({ register, index }) => {
           {...register(`menu.mains[${index}].type`)}
           value="mains"
         />
-         <input
+        <input
           type="hidden"
           {...register(`menu.mains[${index}].menuphoto`)}
           value="mains"
         />
+        <Button
+          style={{
+            backgroundColor: "#feaa00",
+            borderColor: "#feaa00",
+            padding: "0.25rem",
+            margin: "3%",
+          }}
+        >
+          <AddImageIcon style={{ width: "30px", height: "30px" }} />
+          Upload Images
+        </Button>
         <br />
-  
+        <Button
+          variant="danger"
+          style={{
+            padding: "0.25rem",
+            margin: "3%",
+            top: "50%",
+            left: "50%",
+          }}
+          // onClick={handleDelete}
+        >
+          <Delete style={{ width: "30px", height: "30px" }} />
+          Delete
+        </Button>
         <br />
-      </Container>
-    );
-  };
 
-export default Main
+        <br />
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Main;
