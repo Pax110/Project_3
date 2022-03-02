8d7f0bf268b5f3adcb9494636dfefbfeb9a6a943
Pax <tripathipankil@gmail.com>
2022-03-01 20:52:20 -0700

    live orders receiving process

--- client/src/components/restaurant/RestoOrdersView.js
+++ client/src/components/restaurant/RestoOrdersView.js
@@ -2,11 +2,23 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useUserAuth } from "../context/UserAuthContext";
 import { useParams } from "react-router-dom";
-import { Button, Card, Col, ListGroup, Nav, NavDropdown, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  ListGroup,
+  Nav,
+  NavDropdown,
+  Row,
+  Tab,
+  Tabs,
+} from "react-bootstrap";
 import { Container } from "@mui/material";
+import RestoOrdersViewAll from "./RestoOrdersViewAll";
 
 const LiveOrders = () => {
   const { db, user } = useUserAuth();
+ 
 
   const [orders, setOrders] = useState();
   const { id } = useParams();
@@ -31,7 +43,7 @@ const LiveOrders = () => {
     };
 
     getData();
-  }, [user.uid]);
+  }, [user.uid, orders]);
   console.log("orders..", orders);
 
   const orderStyle = {
@@ -41,79 +53,36 @@ const LiveOrders = () => {
   const myStyle = { fontFamily: "Bebas Neue" };
 
   const handleSelect = () => {
-    console.log("some link selected")
-  }
+    console.log("some link selected");
+  };
   if (orders) {
-    return (<>
-     
-      <Container
-        style={{
-          width: "auto",
-          backgroundColor: "#f7f4ef",
-          borderRadius: "15px",
-          paddingBottom: "15px",
-          overflowY: "auto",
-          maxHeight: "800px",
-        }}
-      >
-         <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="#/home">
-          NavLink 1 content
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
-          NavLink 2 content
-        </Nav.Link>
-      </Nav.Item>
-     
-      <NavDropdown title="Dropdown" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-        <h1 className="p-4 box mt-3 text-center" style={myStyle}>
-          Order Tracker
-        </h1>
-        {orders.map((order) => (
-          <div>
-            <Card
-              style={{ width: "50rem", margin: "auto", marginBottom: "10px" }}
-            >
-              <Card.Body>
-                <Row>
-                  <Col>
-                    <Card.Title>Order Number: {order.orderId}</Card.Title>
-                    <span> Date: {order.orderDate}</span> <br />
-                  </Col>
-                  <Col style={{ textAlign: "right" }}>
-                    <strong>Status: Pending</strong> <br />
-                    <strong>Delivery Type: {order.deliveryType}</strong> <br />
-                  </Col>
-                </Row>
-                <Card.Text>
-                  Items: <br />
-                  {order.orderItems.map((item) => (
-                    <>
-                      <span>{item.qty}</span>
-                      <span> x </span>
-                      <span>{item.name}</span>
-                      <br></br>
-                    </>
-                  ))}
-                  <hr></hr>
-                  <p>Order Total: ${order.orderTotal}</p>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
-      </Container>
-      </>);
+    return (
+      <>
+        <Container
+          style={{
+            width: "auto",
+            backgroundColor: "#f7f4ef",
+            borderRadius: "15px",
+            paddingBottom: "15px",
+            overflowY: "auto",
+            maxHeight: "800px",
+          }}
+        >
+          <Tabs
+            defaultActiveKey="All"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+          >
+            <Tab eventKey="Live" title="Live Orders">
+              {/* {Live orders would go here...} */}
+            </Tab>
+            <Tab eventKey="All" title="All">
+              <RestoOrdersViewAll orders={orders}/>
+            </Tab>
+          </Tabs>
+        </Container>
+      </>
+    );
   } else {
     return (
       <>
