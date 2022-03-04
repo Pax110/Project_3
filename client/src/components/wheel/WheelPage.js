32653ac34e57864b5c5829a8b0b492a8ae08810c
Michlovin <michb5@hotmail.com>
2022-03-04 12:04:24 -0700

    wheel

--- client/src/components/wheel/WheelPage.js
+++ client/src/components/wheel/WheelPage.js
@@ -4,18 +4,16 @@ import Wheel from "./wheel";
 
 const WheelPage = () => {
   return (
-    <div>
-      <Container
-        style={{
-          width: "400px",
-          backgroundColor: "#f7f4ef",
-          borderRadius: "15px",
-          paddingBottom: "15px",
-        }}
-      >
-        <Wheel />
-      </Container>
-    </div>
+    <Container
+      style={{
+        width: "600px",
+        backgroundColor: "#f7f4ef",
+        borderRadius: "15px",
+        paddingBottom: "15px",
+      }}
+    >
+      <Wheel />
+    </Container>
   );
 };
 
