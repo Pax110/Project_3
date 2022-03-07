5792b9d3769561e2116e6e5a6d3f996764d0b38a
kpow07 <kpow@shaw.ca>
2022-03-07 10:02:13 -0700

    driver dash

--- client/src/components/driver/DriverDash.js
+++ client/src/components/driver/DriverDash.js
@@ -1,19 +1,45 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Map from "../map/Map";
 import { Link } from "react-router-dom";
+import background from "../landingimage/dash.jpeg";
 
 const DriverDash = () => {
+  const myStyle = {
+    fontFamily: "Bebas Neue",
+    textAlign: "center",
+    textDecoration: "none",
+  };
   return (
-    <>
-      <div>DriverDash</div>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        padding: "20px",
+        height: "100%",
+      }}
+    >
+      <Container
+        style={{
+          width: "auto",
+          height: "100vh",
+          backgroundColor: "#f7f4ef",
+          borderRadius: "15px",
+          padding: "2%",
+        }}
+      >
+        <h1 className style={myStyle}>
+          Driver Dashboard
+        </h1>
 
-      <Link to="/driver/orders">
-        <Button>View Orders</Button>
-      </Link>
+        <Link to="/driver/orders">
+          <Button style={{ marginBottom: "20px" }}>View Orders</Button>
+        </Link>
 
-      <Map />
-    </>
+        <Map />
+      </Container>
+    </div>
   );
 };
 
