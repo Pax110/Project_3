81d54d4c94c6596021792bf707bb7bc821b3bfbb
Pax <tripathipankil@gmail.com>
2022-03-08 23:58:53 -0700

    mailto Button added

--- client/src/components/admin/SingleUser.jsx
+++ client/src/components/admin/SingleUser.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserSettings } from "../icon/UserSettings";
+import ButtonMailto from "./ButtonMailto";
 
 const SingleUser = ({ index, data }) => {
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState();
 
   return (
     <>
@@ -15,12 +16,11 @@ const SingleUser = ({ index, data }) => {
       </td>
       <td>{data.firstName}</td>
       <td>{data.lastName}</td>
-      <a href="mailto:?subject=Culinary Collective <3 &amp;body=Hello, We would like to hear about your feeback. :)">
-      <td style={{ padding: "0.5rem", width: "auto" }}>
-        {data.email}
-      </td>{" "}</a>
+      <td>
+        <ButtonMailto label={data.email} mailto={`mailto:${data.email}`} />
+      </td>
+
       <td>{data.phone}</td>
-     
     </>
   );
 };
