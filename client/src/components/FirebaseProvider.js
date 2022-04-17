062d0c8f9e3ea6db3fb5796874ecbedfa38de8bc
Pax <tripathipankil@gmail.com>
2022-04-16 23:14:23 -0600

    rmv

--- client/src/components/FirebaseProvider.js
+++ client/src/components/FirebaseProvider.js
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 export const FirebaseContext = React.createContext({});
 
 const config = {
-  apiKey: "AIzaSyDa0LD1FY_-s8G9Sq__4jyQaHnCtwVs4O4",
-  authDomain: "project-c1c8c.firebaseapp.com",
-  projectId: "project-c1c8c",
-  storageBucket: "project-c1c8c.appspot.com",
-  messagingSenderId: "828436672252",
-  appId: "1:828436672252:web:42afbb782a929e5f497179",
-  measurementId: "G-D18T0N5LVG",
+ 
 };
 const app = initializeApp(config);
 //console.log(`config is : `, config);
